95be10c77c942ad1c2828c7de33f957b6ef149ba
AzazelloKAY <kokoshkoa@gmail.com>
2018-01-18 19:21:22 +0200

    18.01.2018

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "sources/ft_printf.h"
 
 int main() {
     printf("Hello, World! m\n");
---
+++ sources/ft_printf.c
@@ -0,0 +1,7 @@
+//
+// Created by Antonin KOKOSHKO on 1/18/18.
+//
+
+#include "ft_printf.h"
+
+int printf(const char *format, ...)
