72bfdbdb5aceee0926c1e46fa70c283fdacc1681
AzazelloKAY <kokoshkoa@gmail.com>
2018-01-22 19:50:48 +0200

    22.01.2018 strjoin modified

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -14,11 +14,19 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char *res;
+	char	*res;
+	size_t	len;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	if (!(res = ft_strnew(ft_strlen(s1) + ft_strlen(s2))))
+	len = 0;
+	len += (s1 != NULL) ? ft_strlen(s1) : 0;
+	len += (s2 != NULL) ? ft_strlen(s2) : 0;
+	if (!(res = ft_strnew(len)))
 		return (NULL);
-	return (ft_strcat(ft_strcat(res, s1), s2));
+	if (s1 != NULL)
+		ft_strcat(res, s1);
+	if (s2 != NULL)
+		ft_strcat(res, s2);
+	return (res);
 }
